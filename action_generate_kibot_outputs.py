4ae77ecae3993e4a4f7c344c0aa590a208cda37a
oomlout <aaron@oomlout.com>
2023-08-04 16:51:21 +0100

    comitting after 50 generations

--- action_generate_kibot_outputs.py
+++ action_generate_kibot_outputs.py
@@ -53,7 +53,7 @@ def go_through_directories(**kwargs):
                             
                             count += 1
                             #print a dot every 100 times through#
-                            if count % 1 == 0:
+                            if count % 50 == 0:
                                 #push to github
                                 print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
                                 print("%%%%%%  pushing to github")
