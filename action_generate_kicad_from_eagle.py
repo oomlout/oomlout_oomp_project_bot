95d27aaa5f0e7d4a352f9e3ab37451e78a6d3b77
Oom Lout <aaron@oomlout.com>
2023-08-07 17:41:08 +0100

    oshcamp update

--- action_generate_kicad_from_eagle.py
+++ action_generate_kicad_from_eagle.py
@@ -3,14 +3,19 @@ import os
 
 def go_through_directories():   
     # go through all directories in projects
+    
+
     for root, dirs, files in os.walk("projects"):
         #go through all files
         for file in files:
             #check for a brd file
             
             filename = os.path.join(root, file)
-            filter = ["electrolama","sparkfun","adafruit","omerk","dangerousprototypes"]
+        
+            #filter = ["electrolama","sparkfun","adafruit","omerk","dangerousprototypes"]
             #filter = ["omerk","electrolama"]
+            filter = ["oshcamp"]
+            #filter = ""
             #if any of filter is in filename
             if any(x in filename for x in filter):                
                 if file.endswith(".brd"):
