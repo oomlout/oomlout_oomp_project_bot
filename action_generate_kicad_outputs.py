fa730023cd28b00d1306a1ddcfcf28b56b4479f0
Oom Lout <aaron@oomlout.com>
2023-08-15 16:22:29 +0000

    comitting after 50 generations

--- action_generate_kicad_outputs.py
+++ action_generate_kicad_outputs.py
@@ -3,20 +3,30 @@ import os
 
 def go_through_directories():
     # go through all directories in projects
+    count = 1
     for root, dirs, files in os.walk("projects"):
         #go through all files
+
         for file in files:
             #check for a brd file
             
             filename = os.path.join(root, file)
             filter = ["sparkfun","adafruit","omerk"]
             filter = ["omerk"]
-            filter = ["oshcamp"]
+            filter = [""]            
             #if any of filter is in filename
             if any(x in filename for x in filter):
                 if file.endswith(".kicad_pcb"):
-                    oom_kicad.genera
-                    te_outputs(filename=filename)
+                    oom_kicad.generate_outputs(filename=filename, computer="surface")
+                    count += 1
+
+                    #if 100 times through
+                    if count % 50 == 0:
+                        oom_kicad.push_to_git(count=count)
+
+                    pass
+    oom_kicad.push_to_git()
+
 
 
 if __name__ == '__main__':
