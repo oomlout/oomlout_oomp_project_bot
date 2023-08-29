b368b306a255de8fb8268581f1a39bc9ec1a058d
Oom Lout <aaron@oomlout.com>
2023-08-29 16:21:59 +0000

    comitting after 5000 generations

--- action_generate_image_resolutions.py
+++ action_generate_image_resolutions.py
@@ -3,32 +3,10 @@ import oom_base
 
 
 def main():
-    #go through all files in symbols/
-    import os
-    count = 1   
-    for root, dirs, files in os.walk("projects"):
-        #for each directory
-        for name in dirs:
-            #go through the files in this directory just one level
-            for file in os.listdir(os.path.join(root, name)):
-                #if kicad_mod file
-                if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
-                    resolutions = [140,300,600,1000]
-                    for resolution in resolutions:
-                        #generate the image at this resolution
-                        filename = os.path.join(root, name, file)
-                        #print(filename)
-                        oom_base.generate_image(filename=filename, resolution=resolution)
-                    pass
-                    count += 1
-                    #print a dot every 1000 files
-                    if count % 100 == 0:
-                        print(".", end="", flush=True)
-                    
-
-
-
-
+    #go through all files in symbols/    
+    directory = "projects"
+    overwrite = False
+    oom_base.image_resolutions_dir(directory=directory, overwrite=overwrite)
 
 
 
--- action_generate_kicad_outputs.py
+++ action_generate_kicad_outputs.py
@@ -19,8 +19,8 @@ def go_through_directories():
                     count += oom_kicad.generate_outputs(filename=filename, computer="surface")
                     pass
                 #commit to git every 1
-                    if count % 100 == 0:
-                        oom_kicad.push_to_git(count = count )
+                    if count % 1 == 0:
+                        oom_kicad.push_to_git(count = count)
                         pass    
     
 
